62437c19e22aa1cb281836de46da64c08c898768
William Walsh <wmdwalsh0@gmail.com>
2021-07-05 22:56:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = "steve";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "asd";
+}
